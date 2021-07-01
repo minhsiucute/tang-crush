14dbc6780d0e515b50977be5e356ef369c2f094d
minhsiucute <86772351+minhsiucute@users.noreply.github.com>
2021-07-01 18:03:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Thư à:3',
+    introTitle: 'Trí à:3',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Yeuu anhh :3',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu anhh <33',
-    btnNo: 'Khong có đâu đừng có mơ :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anhh <3',
+    btnIntro: 'ju wa i :3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'đúng gòi đó <3 ',
+    btnNo: 'Đéo :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Ai bỉu đáng iu quá chi ><',
-    mess: 'oroo,yeuu em :33',
-    messDesc: 'Tối nay 9h call với anh nha<3.',
+    reply: 'Bỏ bùa ><',
+    mess: 'yêu anh nhiềuu :33',
+    messDesc: 'Tối nay 8h call video với em nha<3.',
     btnAccept: 'Okiiiii lun <33',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
